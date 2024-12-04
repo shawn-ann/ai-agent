28790b2ab326a22e484a91724a935fafa1117902
shuai.an <shuai.an@rakuten.com>
2024-12-04 17:28:07 +0800

    test

--- src/main.py
+++ src/main.py
@@ -1,44 +1,47 @@
-from langchain_ollama import ChatOllama
 from langchain_core.messages import AIMessage
 from typing import List
 from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
+from pydantic import BaseModel, Field
+
+
+from typing_extensions import Annotated, TypedDict
+
+@tool
+def add(a: int, b: int) -> int:
+    """Add two integers.
+
+    Args:
+        a: First integer
+        b: Second integer
+    """
+    return a + b
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two integers.
+
+    Args:
+        a: First integer
+        b: Second integer
+    """
+    return a * b
+
+
+tools = [add, multiply]
+
+
 
 llm = ChatOllama(
     model="llama3.2",
     temperature=0,
     base_url="http://localhost:11434",
 )
+llm_with_tools = llm.bind_tools(tools, tool_choice="multiply")
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that answer users question.",
-    ),
-    ("human", "今天天气怎么样？"),
-]
-ai_msg = llm.invoke(messages)
-print(ai_msg)
 
-# @tool
-# def validate_user(user_id: int, addresses: List[str]) -> bool:
-#     """Validate user using historical addresses.
-#
-#     Args:
-#         user_id (int): the user ID.
-#         addresses (List[str]): Previous addresses as a list of strings.
-#     """
-#     return True
-#
-#
-# llm = ChatOllama(
-#     model="llama3.2",
-#     temperature=0,
-# ).bind_tools([validate_user])
-#
-# result = llm.invoke(
-#     "Could you validate user 123? They previously lived at "
-#     "123 Fake St in Boston MA and 234 Pretend Boulevard in "
-#     "Houston TX."
-# )
-# result.tool_calls
+
+query = "What is 3 * 12?"
+result = llm_with_tools.invoke(query)
+print(result)
+print(result.tool_calls)
